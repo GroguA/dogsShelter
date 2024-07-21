2b9c32d263b5e2ac3574b4082c36a71f3e045e85
GroguA <117662071+GroguA@users.noreply.github.com>
2024-07-21 19:13:14 +1200

    Add text field factory

--- Factories/TextFieldsFactory.swift
+++ Factories/TextFieldsFactory.swift
@@ -5,4 +5,17 @@
 //  Created by Александра Сергеева on 21.07.2024.
 //
 
-import Foundation
+import UIKit
+
+final class TextFieldsFactory {
+    static func createTextField(with placeholder: String?) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = placeholder
+        textField.keyboardType = .default
+        textField.autocapitalizationType = .words
+        textField.borderStyle = .roundedRect
+        textField.autocorrectionType = .no
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }
+}

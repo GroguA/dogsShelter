5bafad0cbba3cc490ea586c7327eef843ef016d3
GroguA <117662071+GroguA@users.noreply.github.com>
2024-07-21 19:12:19 +1200

    Add navigation to add new dog screen

--- AddNewDogScreen/Assembly/AddNewDogAssembly.swift
+++ AddNewDogScreen/Assembly/AddNewDogAssembly.swift
@@ -1,8 +1,17 @@
 //
-//  AddNewDogAssembly.swift
+//  AddNewDogScreenAssembly.swift
 //  Shelter
 //
 //  Created by Александра Сергеева on 21.07.2024.
 //
 
-import Foundation
+import UIKit
+
+enum AddNewDogScreenAssembly {
+    static func createAddNewDogModule(with navigationController: UINavigationController) -> UIViewController {
+        let router = AddNewDogRouter(navigationController: navigationController)
+        let viewModel = AddNewDogViewModel(router: router)
+        let view = AddNewDogViewController(viewModel: viewModel)
+        return view
+    }
+}
--- AddNewDogScreen/ViewModel/AddNewDogViewModel.swift
+++ AddNewDogScreen/ViewModel/AddNewDogViewModel.swift
@@ -7,24 +7,58 @@
 
 import Foundation
 
-class AddNewDogViewModel {
-    
+protocol IAddNewDogViewModel {
+    var onAction: (AddNewDogAction) -> () { get set }
+    func onAddDogButtonClicked(name: String?, breed: String?, dateOfBirth: String?, image: Data?)
+}
+
+protocol IAddNewDogNavigation {
+    func navigateToSelectBreedScreen(doOnSingleSelect: @escaping (String) -> Void)
+}
+
+class AddNewDogViewModel: IAddNewDogViewModel {
     var onAction: (AddNewDogAction) -> ()  = { _ in }
     
     private let dogStorage = DogStorageService.shared
     
-    func onAddDogBtnClicked(name: String?, breed: String?, dateOfBirth: String?, image: Data?) {
-        guard let name = name?.trimmingCharacters(in: .whitespaces), !name.isEmpty, let breed = breed, !breed.isEmpty, let dateOfBirth = dateOfBirth, !dateOfBirth.isEmpty, let image = image, !image.isEmpty else {
+    private let router: AddNewDogRouter
+    
+    init(router: AddNewDogRouter) {
+        self.router = router
+    }
+    
+    func onAddDogButtonClicked(name: String?, breed: String?, dateOfBirth: String?, image: Data?) {
+        guard let name = name?.trimmingCharacters(in: .whitespaces),
+                !name.isEmpty,
+              let breed = breed,
+              !breed.isEmpty,
+              let dateOfBirth = dateOfBirth,
+              !dateOfBirth.isEmpty,
+              let image = image,
+              !image.isEmpty
+        else {
             onAction(AddNewDogAction.showError(text: "Please fill all text fields"))
             return
         }
-        let dog = SaveDogCoreDataModel(name: name, breed: breed, dateOfBirth: dateOfBirth, image: image)
+        
+        let dog = SaveDogCoreDataModel(
+            name: name,
+            breed: breed,
+            dateOfBirth: dateOfBirth,
+            image: image
+        )
+        
         if dogStorage.saveDog(dog: dog) {
             onAction(AddNewDogAction.closeScreen)
         } else {
             onAction(AddNewDogAction.showError(text: "Can't save this dog"))
         }
-        
     }
     
+}
+
+extension AddNewDogViewModel: IAddNewDogNavigation {
+    func navigateToSelectBreedScreen(doOnSingleSelect: @escaping (String) -> Void) {
+        router.showSelectBreedScreen(doOnSingleSelect: doOnSingleSelect)
+    }
 }

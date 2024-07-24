39466150781b5b96e899535a4f714520f641713f
GroguA <117662071+GroguA@users.noreply.github.com>
2024-07-24 14:39:37 +1200

    Refactor dog details screen

--- DogDetailsScreen/Router/DogDetailsRouter.swift
+++ DogDetailsScreen/Router/DogDetailsRouter.swift
@@ -5,4 +5,32 @@
 //  Created by Александра Сергеева on 23.07.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol IDogDetailsRouter {
+    func popViewController()
+    func navigateToScheduleReminderScreen()
+}
+
+final class DogDetailsRouter: IDogDetailsRouter {
+    private let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func popViewController() {
+        navigationController.popViewController(animated: true)
+    }
+    
+    func navigateToScheduleReminderScreen() {
+        let reminderVC = ScheduleReminderViewController()
+        let isNotificationOn = DogsNotificationsManager.shared.getAvailability()
+        if isNotificationOn {
+            navigationController.pushViewController(reminderVC, animated: true)
+//            reminderVC.dogId = self.dogId
+        } else {
+            return
+        }
+    }
+}
--- DogDetailsScreen/View/DogDetailsViewController.swift
+++ DogDetailsScreen/View/DogDetailsViewController.swift
@@ -8,143 +8,9 @@
 import UIKit
 
 class DogDetailsViewController: UIViewController {
+    private var viewModel: IDogDetailsViewModel & IDogDetailsNavigation
     
-    var dogId = ""
-    
-    private let viewModel = DogDetailsViewModel()
-    
-    private lazy var infoStackView: UIStackView = {
-        let view = UIStackView()
-        view.axis = .vertical
-        view.spacing = 16
-        view.distribution = .equalSpacing
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private lazy var titlesStackView: UIStackView = {
-        let view = UIStackView()
-        view.axis = .vertical
-        view.spacing = 16
-        view.distribution = .equalSpacing
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    private lazy var dogImage: UIImageView = {
-        let image = UIImageView(frame: .zero)
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.contentMode = .scaleToFill
-        image.layer.masksToBounds = true
-        image.layer.cornerRadius = 5
-        return image
-    }()
-    
-    private lazy var dogName: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        return label
-    }()
-    
-    private lazy var dogBreed: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        return label
-    }()
-    
-    private lazy var dogAge: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        return label
-    }()
-    
-    private lazy var name: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20, weight: .medium)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        label.text = "Name: "
-        return label
-    }()
-    
-    private lazy var breed: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20, weight: .medium)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        label.text = "Breed: "
-        return label
-    }()
-    
-    private lazy var age: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.font = .systemFont(ofSize: 20, weight: .medium)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        label.text = "Age: "
-        return label
-    }()
-    
-    private lazy var dateOfWashTitle: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 20, weight: .medium)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        label.numberOfLines = 0
-        label.text = "Dog's last wash: "
-        return label
-    }()
-    
-    private lazy var lastDogsWash: UILabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 20)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = .white
-        label.textColor = .black
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private lazy var updateWashDateButton: UIButton = {
-        var configuration = UIButton.Configuration.filled()
-        configuration.baseBackgroundColor = .systemBlue
-        configuration.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 5)
-        let button = UIButton(configuration: configuration)
-        button.setTitle("Wash dog", for: .normal)
-        button.tintColor = .white
-        button.configuration?.titleTextAttributesTransformer =
-        UIConfigurationTextAttributesTransformer { incoming in
-            var outgoing = incoming
-            outgoing.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-            return outgoing
-        }
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 8
-        button.isEnabled = true
-        button.setTitleColor(.black, for: .disabled)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(washDogClicked), for: .touchUpInside)
-        return button
-    }()
+    private let containerView = DogDetailsContainerView()
     
     private lazy var menuItems: [UIAction] = {
         return [
@@ -167,112 +33,76 @@ class DogDetailsViewController: UIViewController {
         return menuButton
     }()
     
+    init(viewModel: IDogDetailsViewModel & IDogDetailsNavigation) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = containerView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         
-        viewModel.getOneDogByID(id: dogId)
-        
-        viewModel.viewStateDidChange = { viewState in
-            self.renderViewState(state: viewState)
+        viewModel.viewStateDidChange = { [weak self] viewState in
+            self?.renderViewState(state: viewState)
         }
         
-        viewModel.onAction = { action in
+        viewModel.onAction = { [weak self] action in
             switch action {
             case .showError:
-                self.showAlert()
+                self?.showAlert()
             case .closeScreen:
-                self.navigationController?.popViewController(animated: true)
+                self?.viewModel.popDogDetailsScreen()
             }
         }
         
-        self.navigationItem.rightBarButtonItem = menuButton
-        
     }
-    
-    private func setupViews() {
-        view.addSubview(dogImage)
-        view.backgroundColor = .white
-        infoStackView.addArrangedSubview(dogName)
-        infoStackView.addArrangedSubview(dogBreed)
-        infoStackView.addArrangedSubview(dogAge)
-        view.addSubview(infoStackView)
-        titlesStackView.addArrangedSubview(name)
-        titlesStackView.addArrangedSubview(breed)
-        titlesStackView.addArrangedSubview(age)
-        view.addSubview(titlesStackView)
-        view.addSubview(dateOfWashTitle)
-        view.addSubview(lastDogsWash)
-        view.addSubview(updateWashDateButton)
-        
+}
+
+private extension DogDetailsViewController {
+    func setupViews() {
+        navigationItem.rightBarButtonItem = menuButton
         navigationItem.title = "Dog info"
         
-        let constraints = [
-            dogImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            dogImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            dogImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            dogImage.widthAnchor.constraint(equalToConstant: view.bounds.width - 32),
-            dogImage.heightAnchor.constraint(equalTo: dogImage.widthAnchor),
-            
-            titlesStackView.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: 16),
-            titlesStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            
-            infoStackView.topAnchor.constraint(equalTo: dogImage.bottomAnchor, constant: 16),
-            infoStackView.leadingAnchor.constraint(lessThanOrEqualTo: titlesStackView.trailingAnchor, constant: 1),
-            infoStackView.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            
-            dateOfWashTitle.topAnchor.constraint(equalTo: titlesStackView.bottomAnchor, constant: 16),
-            dateOfWashTitle.trailingAnchor.constraint(equalTo: lastDogsWash.leadingAnchor, constant: -1),
-            dateOfWashTitle.leadingAnchor.constraint(equalTo: titlesStackView.leadingAnchor),
-            
-            lastDogsWash.topAnchor.constraint(equalTo: infoStackView.bottomAnchor, constant: 16),
-            lastDogsWash.leadingAnchor.constraint(equalTo: dateOfWashTitle.trailingAnchor, constant: 16),
-            lastDogsWash.trailingAnchor.constraint(greaterThanOrEqualTo: updateWashDateButton.leadingAnchor, constant: -16),
-            
-            updateWashDateButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            updateWashDateButton.topAnchor.constraint(equalTo: infoStackView.bottomAnchor)
-            
-        ]
-        
-        NSLayoutConstraint.activate(constraints)
-        
+        containerView.updateWashDateButton.addTarget(self, action: #selector(washDogClicked), for: .touchUpInside)
     }
     
-    private func renderViewState(state: DogDetailsState) {
+    func renderViewState(state: DogDetailsState) {
         switch state {
         case .success(let dog):
-            dogName.text = dog.name
-            dogBreed.text = dog.breed
-            dogAge.text = dog.age
-            dogImage.image = UIImage(data: dog.image)
-            lastDogsWash.text = dog.dateOfWash
+            containerView.dogNameLabel.text = dog.name
+            containerView.dogBreedLabel.text = dog.breed
+            containerView.dogAgeLabel.text = dog.age
+            containerView.dogImage.image = UIImage(data: dog.image)
+            containerView.lastDogsWashLabel.text = dog.dateOfWash
         }
     }
     
-    @objc private func deleteDogTapped() {
-        viewModel.deleteDogClicked(id: dogId)
+    @objc func deleteDogTapped() {
+        viewModel.deleteDogClicked()
     }
     
     
-    @objc private func washDogClicked() {
+    @objc func washDogClicked() {
         viewModel.updateDogWashClicked()
-        self.updateWashDateButton.setTitle("Dog washed", for: .normal)
-        self.updateWashDateButton.isEnabled = false
-        self.updateWashDateButton.backgroundColor = .systemGray
+        containerView.updateWashDateButton.setTitle("Dog washed", for: .normal)
+        containerView.updateWashDateButton.isEnabled = false
+        containerView.updateWashDateButton.backgroundColor = .systemGray
     }
     
-    @objc private func scheduleReminderTapped() {
-        let reminderVC = ScheduleReminderViewController()
-        let isNotificationOn = DogsNotificationsManager.shared.getAvailability()
-        if isNotificationOn {
-            navigationController?.pushViewController(reminderVC, animated: true)
-            reminderVC.dogId = self.dogId
-        } else {
-            return
-        }
+    @objc func scheduleReminderTapped() {
+        viewModel.navigateToScheduleReminderScreen()
     }
     
-    private func showAlert() {
+    func showAlert() {
         let alert = UIAlertController(title: "Error", message: "Error", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
             if action.style == .default {
